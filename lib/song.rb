b40dc7296873f9538f0469590310a891de4b2d9d
GladysKigen <gladys.kigen@student.moringa.com>
2023-02-15 15:38:53 +0300

    completed

--- lib/song.rb
+++ lib/song.rb
@@ -12,5 +12,32 @@ class Song
         @name = name
         @artist = artist
         @genre = genre
-        @count = count
-        
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    #using class method to expose the content of the class variables
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        #contain only unique artsists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+     
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+end
+#Song.new('hit me baby one more time', 'Brittany Spears', 'pop')
